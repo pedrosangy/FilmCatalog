721f9483640079b514dbb66d1514cd0661e27c4e
pedrosangy <pedrosangy@gmail.com>
2025-05-04 13:50:19 -0400

    fix: clean package.json and remove react-scripts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.scss";
 import Navbar from "@/components/Navbar";
- 
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,7 +27,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         <Navbar />
-        
+
         {children}
       </body>
     </html>
